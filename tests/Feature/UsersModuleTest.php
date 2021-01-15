93074d387d7c58d32e0ccc165351e3443d801560
Carlos Abrisqueta <cmabris@gmail.com>
2021-01-15 19:37:43 +0000

    Fin video 21

--- tests/Feature/UsersModuleTest.php
+++ tests/Feature/UsersModuleTest.php
@@ -2,15 +2,27 @@
 
 namespace Tests\Feature;
 
+use App\User;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UsersModuleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     function it_shows_the_users_list()
     {
+        factory(User::class)->create([
+            'name' => 'Joel',
+        ]);
+
+        factory(User::class)->create([
+            'name' => 'Ellie',
+        ]);
+
         $this->get('/usuarios')
             ->assertStatus(200)
             ->assertSee('Listado de usuarios')
