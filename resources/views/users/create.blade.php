9ca3a95ab128cf3573f2b22ab4fa245948d1eddf
Carlos Abrisqueta <cmabris@gmail.com>
2021-01-19 15:43:19 +0000

    Fin clase 28

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,6 +8,15 @@
     <form method="post" action="{{ route('users.store') }}">
         {{ csrf_field() }}
 
+        <label for="name">Nombre:</label>
+        <input type="text" name="name" placeholder="Nombre">
+        <br>
+        <label for="email">Correo electrónico:</label>
+        <input type="email" name="email" placeholder="Correo electrónico">
+        <br>
+        <label for="password">Contraseña:</label>
+        <input type="password" name="password" placeholder="Al menos 6 caracteres">
+        <br>
         <button type="submit">Crear usuario</button>
     </form>
 
