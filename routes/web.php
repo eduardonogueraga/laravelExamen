d0f450d093ef44b6b292ac347891c748290f5e22
Carlos Abrisqueta <cmabris@gmail.com>
2021-01-21 17:01:30 +0000

    Fin clase 33

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/usuarios', 'UserController@index')->name('users.index');
 Route::get('usuarios/nuevo', 'UserController@create')->name('users.create');
 Route::post('/usuarios', 'UserController@store')->name('users.store');
 Route::get('/usuarios/{user}/editar', 'UserController@edit')->name('users.edit');
+Route::put('/usuarios/{user}', 'UserController@update')->name('users.update');
 
 Route::get('usuarios/{user}', 'UserController@show')->where('user', '[0-9]+')->name('users.show');
 
