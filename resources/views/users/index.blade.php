9bd179353bcb3dc14abe4abed7a258899aa68303
Carlos Abrisqueta <cmabris@gmail.com>
2021-01-22 19:23:55 +0000

    Fin clase 38

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,11 +6,19 @@
 
 @section('content')
 <h1>{{ $title }}</h1>
-
+<p>
+    <a href="{{ route('users.create') }}">Nuevo usuario</a>
+</p>
 <ul>
     @forelse($users as $user)
-        <li>{{ $user->name }} ({{ $user->email }})</li>
-        <a href="{{ route('users.show', $user->id) }}">Ver detalles</a>
+        <li>{{ $user->name }} ({{ $user->email }})
+            <a href="{{ route('users.show', $user) }}">Ver detalles</a>
+            <a href="{{ route('users.edit', $user) }}">Editar</a>
+            <form action="{{ route('users.destroy', $user) }}" method="post">
+                {{ csrf_field() }} {{ method_field('delete') }}
+                <button type="submit">Eliminar</button>
+            </form>
+        </li>
     @empty
         <p>No hay usuarios registrados</p>
     @endforelse
