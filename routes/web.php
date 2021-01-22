e3c9c18cda71b738bd27570a72e875e7127ffc3e
Carlos Abrisqueta <cmabris@gmail.com>
2021-01-22 19:00:57 +0000

    Fin clase 37

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('usuarios/nuevo', 'UserController@create')->name('users.create');
 Route::post('/usuarios', 'UserController@store')->name('users.store');
 Route::get('/usuarios/{user}/editar', 'UserController@edit')->name('users.edit');
 Route::put('/usuarios/{user}', 'UserController@update')->name('users.update');
-
+Route::delete('/usuarios/{user}', 'UserController@destroy')->name('users.destroy');
 Route::get('usuarios/{user}', 'UserController@show')->where('user', '[0-9]+')->name('users.show');
 
 Route::get('/saludo/{name}/{nickname?}', 'WelcomeUserController');
--- tests/Feature/UsersModuleTest.php
+++ tests/Feature/UsersModuleTest.php
@@ -303,4 +303,18 @@ class UsersModuleTest extends TestCase
         ]);
     }
 
+    /** @test */
+    function it_deletes_a_user()
+    {
+        $this->withoutExceptionHandling();
+        $user = factory(User::class)->create();
+
+        $this->delete('/usuarios/'.$user->id)
+            ->assertRedirect('/usuarios');
+
+        $this->assertDatabaseMissing('users', [
+            'id' => $user->id,
+        ]);
+    }
+
 }
