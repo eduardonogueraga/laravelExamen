5d368b5236607812dd7e077226f767112b84477e
Carlos Abrisqueta <cmabris@gmail.com>
2021-02-15 13:04:54 +0000

    Fin clase 26 de nuevo

--- tests/Feature/Admin/SearchUsersTest.php
+++ tests/Feature/Admin/SearchUsersTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Feature\Admin;
 
-use App\User;
+use App\{User, Team};
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -76,6 +76,56 @@ class SearchUsersTest extends TestCase
             ->assertViewHas('users', function ($users) use ($joel, $ellie) {
                 return $users->contains($joel) && ! $users->contains($ellie);
             });
+    }
+
+    /** @test */
+    function search_users_by_team_name()
+    {
+        $joel = factory(User::class)->create([
+            'name' => 'Joel',
+            'team_id' => factory(Team::class)->create(['name' => 'Smuggler'])->id,
+        ]);
+        $ellie = factory(User::class)->create([
+            'name' => 'Ellie',
+            'team_id' => null,
+        ]);
+        $marlene = factory(User::class)->create([
+            'name' => 'Marlene',
+            'team_id' => factory(Team::class)->create(['name' => 'Firefly'])->id,
+        ]);
+
+        $response = $this->get('/usuarios?search=Firefly')
+            ->assertStatus(200);
+
+        $response->assertViewCollection('users')
+            ->contains($marlene)
+            ->notContains($joel)
+            ->notContains($ellie);
+    }
+
+    /** @test */
+    function partial_search_users_by_team_name()
+    {
+        $joel = factory(User::class)->create([
+            'name' => 'Joel',
+            'team_id' => factory(Team::class)->create(['name' => 'Smuggler'])->id,
+        ]);
+        $ellie = factory(User::class)->create([
+            'name' => 'Ellie',
+            'team_id' => null,
+        ]);
+        $marlene = factory(User::class)->create([
+            'name' => 'Marlene',
+            'team_id' => factory(Team::class)->create(['name' => 'Firefly'])->id,
+        ]);
+
+        $response = $this->get('/usuarios?search=Fire')
+            ->assertStatus(200);
+
+        $response->assertViewCollection('users')
+            ->contains($marlene)
+            ->notContains($joel)
+            ->notContains($ellie);
     }
 
 }
