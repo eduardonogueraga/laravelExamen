7356e0f107841a9ea9a19914f6d5f2cee69c9c85
Carlos Abrisqueta <cmabris@gmail.com>
2021-02-15 19:20:56 +0000

    Fin paso 32

--- tests/Feature/Admin/FilterUsersTest.php
+++ tests/Feature/Admin/FilterUsersTest.php
@@ -13,6 +13,7 @@ class FilterUsersTest extends TestCase
     /** @test */
     function filter_users_by_state_active()
     {
+        $this->markTestIncomplete();
         $activeUser = factory(User::class)->create();
         $inactiveUser = factory(User::class)->create();
 
@@ -26,6 +27,7 @@ class FilterUsersTest extends TestCase
     /** @test */
     function filter_users_by_state_inactive()
     {
+        $this->markTestIncomplete();
         $activeUser = factory(User::class)->create();
         $inactiveUser = factory(User::class)->create();
 
